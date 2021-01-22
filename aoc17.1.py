add1b4d45de38992fdee9f2e07e0e6397d34bf8d
FPAM <fpam@novonordisk.com>
2021-01-22 16:21:46 +0100

    work on day 17

--- aoc17.1.py
+++ aoc17.1.py
@@ -51,7 +51,7 @@ def find_if_extension_needed(state:list):
         if expansion_dimensions[5] == True:
             break
 
-    # y axis (?)
+    # y axis (+)
     for z_plane in state:
         for x_cube in z_plane[0]:
             if x_cube == '#':
@@ -61,7 +61,7 @@ def find_if_extension_needed(state:list):
         if expansion_dimensions[2] == True:
             break
     
-    # y asix (?)
+    # y asix (-)
     for z_plane in state:
         for x_cube in z_plane[-1]:
             if x_cube == '#':
@@ -95,7 +95,7 @@ def find_if_extension_needed(state:list):
 # Enlarge the field according to instructions
 def enlarge_field(state: list, dimensions: (bool, bool, bool, bool, bool, bool)) -> list:
 
-    updated_state = state.copy()
+    updated_state = copy.deepcopy(state)
 
     # new z plane -
     if dimensions[5]:
@@ -105,6 +105,7 @@ def enlarge_field(state: list, dimensions: (bool, bool, bool, bool, bool, bool))
         
         updated_state = updated_state + [new_z_plane]
 
+    state = copy.deepcopy(updated_state)
     # new z plane +
     if dimensions[4]:
         new_z_plane = []
@@ -113,23 +114,26 @@ def enlarge_field(state: list, dimensions: (bool, bool, bool, bool, bool, bool))
 
         updated_state = [new_z_plane] + updated_state
 
-    state = updated_state.copy()
+    state = copy.deepcopy(updated_state)
     # new y plane -
     if dimensions[3]:
         for index, z_plane in enumerate(state):
             updated_state[index] = updated_state[index] + [['.' for _ in range(len(state[0][0]))]]
 
+    state = copy.deepcopy(updated_state)
     # new y plane +
     if dimensions[2]:
         for index, z_plane in enumerate(state):
             updated_state[index] = [['.' for _ in range(len(state[0][0]))]] + updated_state[index]
 
+    state = copy.deepcopy(updated_state)
     # new x plane -
-    state = updated_state.copy()
     if dimensions[1]:
         for index_z, z_plane in enumerate(state):
             for index_y, y_row in enumerate(z_plane):
                 updated_state[index_z][index_y] = ['.'] + updated_state[index_z][index_y]
+
+    state = updated_state.copy()
 
     # new x plane +
     if dimensions[0]:
@@ -139,8 +143,27 @@ def enlarge_field(state: list, dimensions: (bool, bool, bool, bool, bool, bool))
 
     return updated_state
 
+# Check how many surrouding cells are active
 def check_active_neighbours(state, x, y, z) -> int:
-    return 0
+    active_neighbours = 0
+
+    for index_z in range(-1,2, 1):
+        if (index_z == -1 and z == 0) or (index_z == 1 and z == len(state) -1):
+            continue
+        else:
+            for index_y in range(-1, 2, 1):
+                if (index_y == -1 and y == 0) or (index_y == 1 and y == len(state[0]) -1):
+                    continue
+                else:
+                    for index_x in range(-1, 2, 1):
+                        if (index_x == -1 and x == 0) or (index_x == 1 and x == len(state[0][0]) -1):
+                            continue
+                        elif index_x == index_y == index_z == 0:
+                            continue
+                        elif state[z+index_z][y+index_y][x+index_x] == '#':
+                            active_neighbours += 1
+
+    return active_neighbours
     
 
 import copy
@@ -151,17 +174,23 @@ def play_round(state: list) -> list:
         for index_y, y_row in enumerate(z_plane):
             for index_x, x_cube in enumerate(y_row):
                 active = check_active_neighbours(state, index_x, index_y, index_z)
+                # print(active, end=' ')
                 if x_cube == '.' and active == 3:
                     new_state[index_z][index_y][index_x] = '#'
                 elif x_cube == '#' and active >= 2 and active <= 3:
                     new_state[index_z][index_y][index_x] = '#'
                 else:
                     new_state[index_z][index_y][index_x] = '.'
+            
+            # print('')
+    
+        # print('')
 
     return new_state
 
-# initial_state = process_input(open_file())
-initial_state = process_input(open_file('17.1.input.test.txt'))
+# updated_state = process_input(open_file())
+updated_state = process_input(open_file('17.1.input.test2.txt'))
+# initial_state = process_input(open_file('17.1.input.test.txt'))
 
 def plane_print(state):
     print('Printing')
@@ -171,11 +200,29 @@ def plane_print(state):
         
         print()
 
-plane_print(initial_state)
+def count_active(state: list) -> int:
+    active_cubes = 0
+    for z_plane in state:
+        for y_row in z_plane:
+            for x_cube in y_row:
+                if x_cube == '#':
+                    active_cubes += 1
+    
+    return active_cubes
 
-updated_state = prepare_field(initial_state)
+# plane_print(initial_state)
 
-plane_print(updated_state)
+# updated_state = prepare_field(initial_state)
 
-updated_state = prepare_field(updated_state)
-plane_print(updated_state)
+# plane_print(updated_state)
+# updated_state = initial_state
+
+for round_number in range(6):
+    
+    updated_state = play_round(prepare_field(updated_state))
+    print(f'Round: {round_number}, active: {count_active(updated_state)}')
+
+    plane_print(updated_state)
+
+# plane_print(updated_state)
+print(count_active(updated_state))
